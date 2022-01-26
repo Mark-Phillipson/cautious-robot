aebcccef543fad7b45ae2960377e33effcf1c1c1
Mark Phillipson <MPhillipson0@Gmail.com>
2022-01-26 09:25:01 +0000

    game options added

---
+++ Client/Models/GameOptions.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Client.Models
+{
+
+public class GameOptions
+{
+
+		public bool ShowOptions{ get; set; } = false;
+		public string? BeginsWith { get; set;}= null;
+
+		[Range(1, 40, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+		public int? MaximumWordLength { get; set;} = 45;
+		public string? APIKey { get; set; } = null;
+	}
+}
