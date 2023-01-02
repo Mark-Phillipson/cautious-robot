3f2c8775fea6a539d635c390751a751c4e998884
Mark Phillipson <MPhillipson0@Gmail.com>
2023-01-02 11:12:35 +0000

    Added Maui app

---
+++ MauiWords/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiWords
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiWords/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiWords
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
